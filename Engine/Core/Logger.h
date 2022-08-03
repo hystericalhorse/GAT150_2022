486ac34f65fbf255f331f926063cbc8aa2d1dbdf
Birdy Bird <edixon@student.neumont.edu>
2022-08-03 10:28:03 -0600

    set global error logging to only operate #ifdef _DEBUG, implemented error messages for AudioSys.

--- Engine/Core/Logger.h
+++ Engine/Core/Logger.h
@@ -1,6 +1,12 @@
 #ifndef _LOGGER_H
 #define _LOGGER_H
 
+#ifdef _DEBUG
+#define LOG(format, ...) en::__logger.Log(format, __VA_ARGS__)
+#else
+#define LOG(format, ...) ((void)0)
+#endif // _DEBUG
+
 namespace en
 {
 	class Logger
