8e6bde6682e355aff28cb9ae4d38602d253ccdfb
Birdy Bird <edixon@student.neumont.edu>
2022-08-03 09:25:42 -0600

    opened new dev branch

--- Engine/Renderer/Renderer.cpp
+++ Engine/Renderer/Renderer.cpp
@@ -24,10 +24,12 @@ namespace en
 		Vector2 size = texture->getSize();
 
 		SDL_Rect dest;
+		// Destination Position
 		dest.x = (int) position.x;
-			dest.y = (int) position.x;
-			dest.w = (int) size.x;
-			dest.h = (int) size.y;
+		dest.y = (int) position.x;
+		// Scale
+		dest.w = (int) size.x;
+		dest.h = (int) size.y;
 
 			SDL_RenderCopyEx(_renderer, texture -> _texture, nullptr, &dest, angle, nullptr, SDL_FLIP_NONE);
 	}
