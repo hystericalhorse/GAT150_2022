bd90aea42bd2ecc36f554cc8426f7b37c89c6dd4
Birdy Bird <edixon@student.neumont.edu>
2022-08-05 10:06:53 -0600

    Implemented RenderComponent and SpriteComponent

---
+++ Engine/Components/SpriteComponent.h
@@ -0,0 +1,19 @@
+#ifndef _COMPONENT_SPRITE_H
+#define _COMPONENT_SPRITE_H
+
+#include "RenderComponent.h"
+
+namespace en
+{
+	class SpriteComponent : public RenderComponent
+	{
+	public:
+		SpriteComponent() = default;
+
+	protected:
+
+
+	};
+}
+
+#endif // _COMPONENT_SPRITE_H
