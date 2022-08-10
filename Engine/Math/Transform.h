143843cb92e1396bef5256b7bb688b7306b5f693
Birdy Bird <edixon@student.neumont.edu>
2022-08-10 10:01:30 -0600

    Added Matrix3x3 and implemented appropriate functions in Transform.

--- Engine/Math/Transform.h
+++ Engine/Math/Transform.h
@@ -2,6 +2,7 @@
 #define _TRANSFORM_H
 #include "Vector2.h"
 #include "Matrix2x2.h"
+#include "Matrix3x3.h"
 #include "MathUtils.h"
 
 namespace en
@@ -17,12 +18,23 @@ namespace en
 		Vector2 scale { 1, 1 };
 		float rotation = 0.0f; // rotation in degrees
 
+		/******************* Legacy ******************
 		operator Matrix2x2 () const
 		{
 			Matrix2x2 mxS = Matrix2x2::scale(scale);
 			Matrix2x2 mxR = Matrix2x2::rotate(en::radians(rotation));
 
 			return { mxS * mxR };
+		}
+		*********************************************/
+
+		operator Matrix3x3 () const
+		{
+			Matrix3x3 mxS = Matrix3x3::scale(scale);
+			Matrix3x3 mxR = Matrix3x3::rotate(en::radians(rotation));
+			Matrix3x3 mxT = Matrix3x3::translate(position);
+
+			return { mxS * mxR * mxT };
 		}
 	};
 }
